88cd4efee72e274cb95e939e5ac58bbfa7aeed6c
danhsama03 <phamcongdanh2003@gmail.com>
2023-04-12 14:56:16 +0700

    Update funtion bai so 4, 5, 6

--- index.ts
+++ index.ts
@@ -38,7 +38,62 @@ function findMaxPrice(books: any): number {
   };
   console.log("ID cua sach co gia cao nhat: " + id);
   return id;
-} 
+}
+
+function isValid(books: any): boolean {
+  let boolean: boolean = false;
+  for (let i: number = 0; i < books.length; i++) {
+    if (books[i]['price'] == 0) {
+      boolean = false;
+      break;
+    } else {
+      boolean = true;
+    };
+    if (books[i]['name'] == '') {
+      boolean = false;
+      break;
+    } else {
+      boolean = true;
+    };
+  };
+  if (boolean == true) {
+    console.log("Mang hop le")
+  } else {
+    console.log("Mang khong hop le")
+  };
+  return boolean;
+}
+
+function filterByPrice(books: any, min: number, max: number): number[] {
+  min = Number(question("Nhap gia tien nho nhat: "));
+  max = Number(question("Nhap gia tien lon nhat: "));
+  let z: number = 0;
+  let books2 : number[] = [];
+  for (let i: number = 0; i < books.length; i++) {
+    if (min <= books[i]['price'] && max >= books[i]['price']) {
+      books2[z] = books[i];
+      z++;
+    };
+  };
+  console.log(books2);
+  return books2;
+}
+
+function searchByName(books: any, keyword: string): number[] {
+  let books3: number[] = [];
+  let x: number = 0;
+  keyword = question("Nhap tu khoa: ");
+  for (let i: number = 0; i < books.length; i++) {
+    for (let y: number = 0; y < books[i]['name'].length; y++) {
+      if (keyword == books[i]['name'][y]) {
+        books3[x] = books[i];
+        x++;
+      };
+    };
+  };
+  console.log(books3);
+  return books3;
+}
 
 function main(): void {
     let publisher: string = "";
@@ -95,6 +150,12 @@ function main(): void {
     countBooksOfPublisher(books, publisher);
     getAvaeragePrice(books);
     findMaxPrice(books);
+    isValid(books);
+    let min: number = 0;
+    let max: number = 0;
+    filterByPrice(books, min, max)
+    let keyword: string = '';
+    searchByName(books, keyword);
 };
 
 main();
